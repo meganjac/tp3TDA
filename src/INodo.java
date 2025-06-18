5f6de2402b2dc3341deddcd3780a172dcf679eef
meganjac <meganr2609@gmail.com>
2025-06-18 18:37:02 -0300

    subiendo los primeros archivos

---
+++ src/INodo.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public interface INodo<T> {
+
+	/*
+	 PRE: -
+	 POST: Lista creada y asociada a cada instancia de INodo
+	 AX: - 
+	 */
+	List<INodo> getVecinos(); //constante
+	
+	/*getters & setters*/
+	T getValor(); //valor de tipo genérico
+	void setValor(T valor);
+	
+
+	/*
+	 *insertar elementos en la lista Vecinos
+	 * PRE: lista creada, independientemente de la cantidad de elementos
+	 * POST: el vecino debe quedar insertado a la lista de adyacencia, como último elemento
+	 * */
+	public void agregarVecino(INodo vecino);
+}
+
